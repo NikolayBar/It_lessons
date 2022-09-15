eaf51b8a711797ab0a7a29e1315bbd6914e51f5d
Nikolay.B <nikolbar@yandex.ru>
2022-09-15 16:00:54 +0300

    add OutLinePlus code

--- DogVsFrends/Program.cs
+++ DogVsFrends/Program.cs
@@ -1,8 +1,9 @@
 ﻿// Сколько раз собака пробежит между идущими 
 // навстречу друг другу друзьями
 int count =0;
-int friend=1;     //друг от которого начнет бегать собачка 
-double dist = 10; //начальная дистанция
+int friendStart=2;        //друг от которого начнет бегать собачка
+int friend = friendStart; //текущий друг
+double dist = 20; //начальная дистанция
 double distEnd=1;    //финишная дистанция
 double firstFriendSpeed=1;
 double secondFriendSpeed=2;
@@ -19,14 +20,25 @@ while (dist > distEnd)
     if (friend == 2)
     {
         time=dist/(secondFriendSpeed+dogSpeed);
-        friend=2;
+        friend=1;
     }
     dist=dist-(firstFriendSpeed+secondFriendSpeed)*time;
     count++;
 }
 
-Console.Clear();
-Console.Write("Собака пробежит ");
+Console.WriteLine(count%2);
+
+if (count%2 ==0)
+{
+    friend=friendStart;
+}
+
+//Console.Clear();
+Console.Write("Стартовав от ");
+Console.Write(friendStart);
+Console.Write("-го друга собака пробежит ");
 Console.Write(count);
-Console.Write(" раз,");
+Console.Write(" раз, окажется около ");
+Console.Write(friend);
+Console.WriteLine("-го друга.");
 
